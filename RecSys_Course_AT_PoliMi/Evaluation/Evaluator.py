1e535e4b40fb85d8a4a1e53e420112bc66f6f66e
filippogalletta <filippo1.galletta@mail.polimi.it>
2026-01-07 17:19:54 +0100

    Fixing compatibility problems with numpy

--- RecSys_Course_AT_PoliMi/Evaluation/Evaluator.py
+++ RecSys_Course_AT_PoliMi/Evaluation/Evaluator.py
@@ -351,7 +351,8 @@ class Evaluator(object):
 
             # Being the URM CSR, the indices are the non-zero column indexes
             recommended_items = recommended_items_batch_list[batch_user_index]
-            is_relevant = np.in1d(recommended_items, relevant_items, assume_unique=True)
+            #is_relevant = np.in1d(recommended_items, relevant_items, assume_unique=True)
+            is_relevant = np.isin(recommended_items, relevant_items, assume_unique=True)
 
             self._n_users_evaluated += 1
 
